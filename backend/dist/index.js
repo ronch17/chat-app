7efd43be6e4c54f3d6813982d4d1431b6af0320a
Ron Dayan <rondayan@Rons-MacBook-Pro.local>
2025-10-08 23:08:51 +0300

    Production ready

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
-    const frontendPath = path.join(__dirname, "../frontend/dist");
+    const frontendPath = path.join(__dirname, "../../frontend/dist");
     app.use(express.static(frontendPath));
     app.use((req, res) => {
         res.sendFile(path.join(frontendPath, "index.html"));
